14d51dddf697cea494b4670f6c1d8c5bea8e6999
King-87 <114271650+King-87@users.noreply.github.com>
2022-09-24 11:45:17 +0700

    Add files via upload

---
+++ klx.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay wong mumet
+echo $green"sugeng rawuh"
+echo
+sleep 2
+echo $yellow"silahkan pilih tools nya :"
+echo
+echo $cyan"1.) stabilkan jaringan"
+echo
+echo $blue"2.) metu mass!!"
+echo
+echo $white
+read -p "mileh ndi! : " mas
+
+if [ $mas = 1 ] || [ $mas = 1 ]
+then
+clear
+toilet -f big -F gay wong mumet
+echo $green "stabil jaringan by wong mumet"
+sleep 2
+ping -s1000 1.1.1.1
+if
+
+if [ $mas = 0 ] || [ $mas = 0 ]
+then
+clear
+echo $yellow
+figlet "dadaa"
+echo $white"To be continued"
+sleep 3
+exit
+if
